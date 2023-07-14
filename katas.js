2369ae2954ce0f0b4482d822ad43366fb56a01f9
BenjiSmith1990 <benmwsmith@gmail.com>
2023-07-14 18:35:48 +0100

    Add completed second problem, two functions, one using frequency counter, the other using multiple pointers

--- katas.js
+++ katas.js
@@ -37,3 +37,46 @@ function sameFrequency(num1, num2){
         return true
     }
   }
+
+// Problem #2
+
+// Frequency Counter - areThereDuplicates
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+
+// Examples:
+
+// areThereDuplicates(1, 2, 3) // false
+// areThereDuplicates(1, 2, 2) // true 
+// areThereDuplicates('a', 'b', 'c', 'a') // true 
+// Restrictions:
+
+// Time Complexity - O(n)
+// Space Complexity - O(n)
+
+  function areThereDuplicates1(...args) {
+   const obj = {}
+
+   for(let item of args){
+       obj[item] = ++obj[item] || 1
+   }
+   for(let key in obj){
+       if(obj[key] > 1) return true
+   }
+       return false
+  }
+
+//   Same problem as above, this time using Multiple Pointers :
+
+function areThereDuplicates2(...args) {
+    args.sort();
+    let start = 0;
+    let next = 1;
+    while(next < args.length){
+      if(args[start] === args[next]){
+          return true
+      }
+      start++
+      next++
+    }
+    return false
+  }
