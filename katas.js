22b0835d3e191c5efe047e0bd16cbf91f296619f
BenjiSmith1990 <benmwsmith@gmail.com>
2023-07-17 14:45:51 +0100

    Add problem #4 issubsequence function using multiple pointer pattern

--- katas.js
+++ katas.js
@@ -115,3 +115,35 @@ function averagePair(arr, target){
     }
     return false
   }
+
+// Problem 4 :
+
+// Multiple Pointers - isSubsequence
+// Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+
+// Examples:
+
+// isSubsequence('hello', 'hello world'); // true
+// isSubsequence('sing', 'sting'); // true
+// isSubsequence('abc', 'abracadabra'); // true
+// isSubsequence('abc', 'acb'); // false (order matters)
+// Your solution MUST have AT LEAST the following complexities:
+
+// Time Complexity - O(N + M)
+
+// Space Complexity - O(1)
+
+function isSubsequence(str1, str2) {
+    
+    if(str1.length > str2.length) return false
+    let startStr1 = 0;
+    for(let i = 0; i <= str2.length; i++){
+        if(startStr1 < str1.length){
+            if(str1[startStr1] === str2[i]){
+                startStr1++
+            }
+        } 
+        else return true
+    }
+        return false
+  }
